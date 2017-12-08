cc02de260a0b1d71e6a0ccc056e4f3ab28fdea03
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-08 15:10:06 -0600

    Created router component for switching

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import { applyMiddleware } from './node_modules/redux/index';
+import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
@@ -20,9 +22,11 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
-          <LoginForm />
+      <Provider store={store}>
+          <Router />
       </Provider>
     );
   }
--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,11 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import { 
+  EMAIL_CHANGED, 
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER 
+} from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -13,3 +20,30 @@ export const passwordChanged = text => {
     payload: text
   };
 };
+
+export const loginUser = ({email, password}) => {
+  return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
+  };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL
+  })
+}
