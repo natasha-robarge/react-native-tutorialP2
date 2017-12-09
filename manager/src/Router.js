f8db02c0b30ed90134169d2e6f65c3a8f0bfaa26
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-09 10:06:23 -0600

    connected employee info to firebase database

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,11 +1,29 @@
 import React, from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm'; 
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
-    <Router>
-      <Scene key="login" component={LoginForm} title="Please login" />
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please login" />
+      </Scene>
+      <Scene key="main">
+        <Scene 
+          rightTitle="Add"
+          onRight={() => Actions.employeeCreate()}
+          key="employeeList" 
+          component={EmployeeList} 
+          title="Employees" 
+          initial
+        />
+        <Scene key="employeeCreate" 
+          component={EmployeeCreate} 
+          title="Create Employee" 
+        />
+      </Scene>
     </Router>
   );
 };
